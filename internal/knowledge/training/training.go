0132d09083e49720e83369b43f7846a3bfd89937
Dem1Kon <konoplev.dm.06@gmail.com>
2025-07-03 09:47:06 +0300

    feat: added new feature: training mode

--- internal/knowledge/training/training.go
+++ internal/knowledge/training/training.go
@@ -1,25 +1,45 @@
 package training
 
 import (
-	"fmt"
 	"github.com/spf13/cobra"
+	"log"
+	"vocabulary/internal/json"
+	"vocabulary/internal/knowledge/test"
 )
 
 // TrainingCmd represents the training command
 var TrainingCmd = &cobra.Command{
 	Use:   "training",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "train of your knowledge",
+	Long: `This command calls a knowledge training
+without influence on word's rating'.`,
+	Example: `vocabulary training
+vocabulary training -m New -a 15`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("training called")
+		test.IsTrainMode = true
+
+		Json, err := json.Init()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		mode, err := cmd.Flags().GetString("mode")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		amount, err := cmd.Flags().GetInt("amount")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		test.Testing(Json, mode, amount)
 	},
 }
 
 func init() {
-
+	TrainingCmd.Flags().BoolVarP(&test.IsNative, "native", "n", false, "Training with native words")
+	TrainingCmd.Flags().BoolVarP(&test.IsForeign, "foreign", "f", false, "Training with foreign words")
+	TrainingCmd.PersistentFlags().IntP("amount", "a", 10, "Flag for set amount of words")
+	TrainingCmd.Flags().StringP("mode", "m", "Any", "Flag for set mode[Level of word's knowledge]")
 }
